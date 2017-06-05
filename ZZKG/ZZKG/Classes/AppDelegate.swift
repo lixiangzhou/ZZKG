cedfbc05c91267de77f36ec99c6c95cdbe34153f
lixiangzhou <lixiangzhouno1@sina.com>
2017-06-05 16:06:43 +0800

    项目结构初始化

---
+++ ZZKG/ZZKG/Classes/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  ZZKG
+//
+//  Created by lixiangzhou on 2017/6/5.
+//  Copyright © 2017年 lixiangzhou. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        
+        window?.backgroundColor = UIColor.white
+        
+        window?.rootViewController = KGNavigationController(rootViewController: MainController())
+        
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
